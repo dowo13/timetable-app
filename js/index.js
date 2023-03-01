2364159f89f4e2ce1332856e1acc0477e89ab8c5
Dominic Hanna <dowo13@gmail.com>
2023-03-01 20:24:17 +0000

    adding modules

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import { ShowDataOnload } from "./showDataOnload.js";
+
+import { AddToCells } from "./addToCells.js";
+
+const runTimeTable = function(){
+    // everything runs from here
+
+    const main = document.getElementById('main');
+    const tableCells = document.querySelectorAll()
+    console.log(main)
+
+    const showSavedDataOnload = new ShowDataOnload();
+    showSavedDataOnload.checkLocalStorage()
+}
+document.addEventListener('DOMContentLoaded', runTimeTable)
---
+++ js/showDataOnload.js
@@ -0,0 +1,8 @@
+export class ShowDataOnload {
+    /**
+     * grab any data from local storage and show on page load
+     */
+    checkLocalStorage(){
+        console.log('i will check local storage and show data on page if anything is in local storage')
+    }
+}
